da26ff7b009f3aed1b3a2020d06320e8526efcdd
Abeyez 7 <154743440+Abeyez7@users.noreply.github.com>
2023-12-24 19:28:29 +0500

    Create 3.conditions.js

---
+++ 3.conditions.js
@@ -0,0 +1,15 @@
+// console.log("Hello this is a conditional tutorial")
+
+let a = prompt("Enter your age")
+// let age=15;
+     
+if(age>18){
+    console.log("you can drive");
+}
+else if(age==18){
+    console.log("your age is 18")
+
+}
+else{
+    console.log("you cannot drive");
+}
