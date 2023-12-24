767b49f0a1a9bdc4ff29a13be79b53633c67b03e
Abeyez 7 <154743440+Abeyez7@users.noreply.github.com>
2023-12-24 19:31:35 +0500

    Create 7.Array.js

---
+++ 7.Array.js
@@ -0,0 +1,94 @@
+@ -0,0 +1,93 @@
+// *\
+
+
+            // Array Function
+
+
+let arr = [1, 2, 3, 4, 5]
+
+// this trick is not use for strings
+
+arr[0] = 544665;  
+console.log(arr)
+// console.log(arr.length)
+// console.log(arr[0])
+// console.log(arr[2])
+// console.log(arr[4])
+
+// console.log(arr.toString())
+// console.log(arr.join(" and "))
+ 
+// return last element of array
+console.log(arr.pop())
+// console.log(arr.push("abeyez"))
+
+
+// shift acts as  out the first element of array
+
+// console.log(arr.shift())
+
+// unshift add element in begining of array
+// console.log(arr.unshift(456))
+
+// delete acts delete the element of array 
+// console.log(arr.delete arr[5])
+
+// let a1 = [1, 2, 5, 6, 3, 9]
+// let a2 = [9, 6, 4, 5, 7, 6]
+// let a3 = [5, 3, 1, 8, 5, 4]
+// print all array in one samitary 
+// console.log(a1.concat(a2,a3))
+// *\
+
+
+
+
+// let arr=[1, 2, 56, 3, 98]
+// for (let index = 0; index < a.length; index++) {
+//     const element = a[index];
+//     console.log(element)
+    
+// }
+
+// a.forEach(e=>{
+
+// })
+
+// map just like insert a new array 
+
+// let newarr = arr.map(e=>{
+//     return e**2
+// })
+// console.log(newarr)
+
+
+// let newarr = arr.map((e , index, array)=>{
+//     return e**2
+// })
+// console.log(newarr)
+
+
+// const greaterThanSix = (e)=>{
+//     if(e>6){
+//         return true
+//     }
+//     return false
+// }
+// console.log(arr.filter(greaterThanSix))
+
+// const greaterThanSeven = (e)=>{
+//     if(e>7){
+//         return true
+//     }
+//     return false
+// }
+// console.log(arr.filter(greaterThanSeven))
+
+// let arr3 = [1,2,3,4,5,6]
+// const red =(a,b)=>{
+//     return a/b
+// }
+// console.log(arr3.reduce(red))
+
+// console.log(arr3.from("abeyex"))
