5e09e78f1ce672cc8fded2be7b16fbc97f810c0c
Abeyez 7 <154743440+Abeyez7@users.noreply.github.com>
2023-12-24 19:29:54 +0500

    Create 5.functions.js

---
+++ 5.functions.js
@@ -0,0 +1,34 @@
+@ -0,0 +1,33 @@
+// functions in javascript
+
+function nice(name) {
+    console.log("hey " + name + " you are nice")
+    console.log("hey " + name + " y ou are intelligent")
+    console.log("hey " + name + " your working speed is very osome")
+    console.log("hey " + name + " you are good teacher")
+}
+name("rehan")
+nice("Ali")
+// console.log("hey " + name + " you are nice")
+
+
+// sum function
+
+
+
+//     function sum(a, b) {
+//         return a + b
+//     }
+    
+   
+
+// result = sum(5+6)
+// console.log("The sum of these number is ", result)
+
+
+
+
+  
+
+
+
