16c665a94842e1b097b419eba2367200e481ae93
Abeyez 7 <154743440+Abeyez7@users.noreply.github.com>
2023-12-24 19:29:11 +0500

    Create 4.loops.js

---
+++ 4.loops.js
@@ -0,0 +1,55 @@
+@ -0,0 +1,55 @@
+// console.log("Hello How are you");
+
+// let a = prompt("Enter the number")
+// for (let i = 0; i < i; i++) {
+//     // const element = 150[i];
+// console.log(a)
+// }
+
+// let obj={
+//     name:"abeyez",
+//     role:"Programmer",
+//     age:"18"
+// }
+// // for in
+// for (const key in obj) 
+//      {
+//         const element=obj[key]
+//         console.log(key,element)
+        
+//     }
+
+
+// for of
+// for (const item of "abeyez") {
+//     console.log(item)
+// }
+
+
+// while loop
+
+// let i=10;
+// while (i<50) {
+//     console.log(i)
+//     i++;    
+// }
+
+
+
+// do while loop
+    
+let i=12;
+do {
+    console.log(i)
+} while (i<50);
+
+
+
+// let num=prompt("Enter the number")
+// let i=0;
+// do {
+    
+// } while (condition);
+
+
