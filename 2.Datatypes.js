eea9fcdcd3537b71ea1432386a2d2ecfa931c0df
Abeyez 7 <154743440+Abeyez7@users.noreply.github.com>
2023-12-24 19:27:44 +0500

    Create 2.Datatypes.js

---
+++ 2.Datatypes.js
@@ -0,0 +1,10 @@
+
+let a = null                           //Object datatype
+let b = 245                            //number datatypes
+let c = true                           //Boolean datatypes
+let d = BigInt("542") + BigInt("8")    //BigInt datatypes
+let e = Symbol("I am a nice symbol")   //Symbol datatypes
+let g = "abeyez"                       //string datatypes
+let h = undefined                      //undefined datatypes
+console.log(a, b, c, d, e, g, h)
+console.log(typeof h)
