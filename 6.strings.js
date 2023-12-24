e1b15feca700d2609e3b5cd6a985951b62879da1
Abeyez 7 <154743440+Abeyez7@users.noreply.github.com>
2023-12-24 19:30:47 +0500

    Create 6.strings.js

---
+++ 6.strings.js
@@ -0,0 +1,30 @@
+@ -0,0 +1,29 @@
+// console.log("This is string tutorial")
+// let a = "abeyez"
+// console.log(a)
+// console.log(a[0])
+// console.log(a[1])
+// console.log(a[2])
+// console.log(a[3])
+// console.log(a[4])
+// console.log(a[5])
+
+// console.log(a.length)
+
+// node templates literals
+
+let name = "abeyez"
+let friend = "ahmad"
+console.log("His name is " + name + " his friend name is " + friend)
+
+console.log(`his name is ${name} and his friend name is ${friend}`)
+
+
+let a= "ahmad"
+console.log(a.toUpperCase())
+console.log(a.toLowerCase())
+console.log(a.slice(1))
+console.log(a.replace("ahmad", "ali"))
+console.log(a.concat(a, "abeyez"))
+
+console.log(a.indexOf(" ma"))
