1527a9576e8a90d3feec9ebd6bc9949a900c267b
Abeyez 7 <154743440+Abeyez7@users.noreply.github.com>
2023-12-24 19:32:30 +0500

    Create 8.DOM.js

---
+++ 8.DOM.js
@@ -0,0 +1,14 @@
+
+console.log("hello world")
+
+
+
+
+document.body
+<body>​…​</body>​<div class=​"container">​…​</div>​<script src=​"script.js">​</script>​<!-- Code injected by live-server --><script>​…​</script>​</body>​
+document.body.firstElementChild
+<div class=​"container">​…​</div>​
+document.body.firstElementChild.childNodes
+NodeList(11) [text, div.box, text, div.box, text, div.box, text, div.box, text, div.box, text]
+document.body.firstElementChild.children
+HTMLCollection(5) [div.box, div.box, div.box, div.box, div.box]
